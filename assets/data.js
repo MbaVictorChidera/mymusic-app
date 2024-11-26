cc26d2b780a9c47cf2a09fc8bd08c1a7add1d173
MbaVictoChidera <mbavictorchidera@gmail.com>
2024-11-26 17:13:38 +0100

    inital commit

---
+++ assets/data.js
@@ -0,0 +1,34 @@
+const musicData = [
+  {
+    id: 1,
+    author: "Paul Wilbur",
+    title: "Blessed Are You - instrumental",
+    imageSrc: "./assets/images/default.png",
+    audioSrc:
+      "./assets/music/Blessed Are You - instrumental karaoke - Paul Wilbur.mp3",
+  },
+  {
+    id: 2,
+    author: "John Wilbur",
+    title: "John Are You - instrumental",
+    imageSrc: "./assets/images/default.png",
+    audioSrc:
+      "./assets/music/Blessed Are You - instrumental karaoke - Paul Wilbur.mp3",
+  },
+  {
+    id: 3,
+    author: "Ovie Wilbur",
+    title: "Ovie Are You - instrumental",
+    imageSrc: "./assets/images/default.png",
+    audioSrc:
+      "./assets/music/Blessed Are You - instrumental karaoke - Paul Wilbur.mp3",
+  },
+  {
+    id: 4,
+    author: "Jane Wilbur",
+    title: "Jane Are You - instrumental",
+    imageSrc: "./assets/images/default.png",
+    audioSrc:
+      "./assets/music/Blessed Are You - instrumental karaoke - Paul Wilbur.mp3",
+  },
+];
---
+++ assets/script.js
@@ -0,0 +1,79 @@
+// alert("hello");
+const song = document.getElementById("song");
+const progress = document.getElementById("progress");
+const playPauseIcon = document.getElementById("play-icon");
+const timer = document.getElementById("timer");
+
+const musicArtist = document.getElementById("music-artist");
+const musicTitle = document.getElementById("music-title");
+const musicImage = document.getElementById("music-image");
+
+let currentIndex = 0;
+
+function populateDetails(index) {
+  const songUpdate = musicData[index];
+  musicArtist.textContent = songUpdate.author;
+  musicTitle.textContent = songUpdate.title;
+  musicImage.src = songUpdate.imageSrc;
+  song.src = songUpdate.audioSrc;
+}
+
+populateDetails(currentIndex);
+
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+
+  return `${minutes}: ${seconds < 10 ? "0" : ""}${seconds} `;
+};
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+
+  timer.textContent = `${formatTime(0)} - ${formatTime(song.duration)}`;
+  song.pause();
+};
+
+const handlePlayPause = () => {
+  if (playPauseIcon.classList.contains("fa-pause")) {
+    song.pause();
+    playPauseIcon.classList.remove("fa-pause");
+    playPauseIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    playPauseIcon.classList.add("fa-pause");
+    playPauseIcon.classList.remove("fa-play");
+  }
+};
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+    timer.textContent = `${formatTime(song.currentTime)} - ${formatTime(
+      song.duration
+    )}`;
+  }, 500);
+}
+
+progress.onchange = () => {
+  song.play();
+  song.currentTime = progress.value;
+  timer.textContent = `${formatTime(song.currentTime)} - ${formatTime(
+    song.duration
+  )}`;
+};
+
+const handleNext = () => {
+  const updateSong = musicData[index];
+  currentIndex = (currentIndex + 1) % updateSong.length;
+
+  populateDetails(currentIndex);
+};
+
+const handlePrevious = () => {
+  const updateSong = musicData[index];
+  currentIndex = (currentIndex - 1) % updateSong.length;
+
+  populateDetails(currentIndex);
+};
